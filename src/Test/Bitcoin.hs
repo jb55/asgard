850b212882788d6bdf746a631fe29f92b6c5d12b
William Casarin <jb55@jb55.com>
2018-01-28 10:07:13 -0800

    rpc: more progress

--- src/Test/Bitcoin.hs
+++ src/Test/Bitcoin.hs
@@ -1,22 +1,38 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Test.Bitcoin (Bitcoin, initBitcoin) where
 
-import Control.Monad (unless)
+import Control.Monad (unless, (<=<), void)
+import Control.Monad.IO.Class (liftIO, MonadIO)
+import Control.Monad.Logger
+import Control.Exception (bracket)
+import UnliftIO (MonadUnliftIO)
+import Control.Concurrent (forkIO)
 import Data.Aeson
-import Data.ByteString (ByteString)
+import Data.ByteString (ByteString, hGetLine)
 import Data.ByteString.Char8 (hPutStrLn)
 import Data.ByteString.Short (ShortByteString)
+import Data.Maybe (fromMaybe)
 import Data.Monoid ((<>))
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
 import Data.Word (Word16, Word8)
 import Network.HTTP.Client
 import Network.HTTP.Types
 import System.Directory (createDirectoryIfMissing)
 import System.FilePath ((</>))
 import System.IO (IOMode(WriteMode), openFile, hClose, Handle)
-import System.Process (createProcess, ProcessHandle)
+import System.Posix.Signals (signalProcess)
+import System.Posix.Types (CPid)
+import System.Process
+import System.Process.Internals
+import System.Timeout (timeout)
+
 import Text.Printf (printf)
 
 import Network.Bitcoin.Api.Wallet
@@ -24,6 +40,8 @@ import Network.Bitcoin.Api.Client (Client(..))
 
 import qualified Data.ByteString.Char8 as B8
 import qualified System.Process as P
+import qualified UnliftIO.Concurrent as UIO
+import qualified UnliftIO.Exception as UIO
 import qualified Data.Aeson as JSON
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.Text.Encoding as TE
@@ -46,20 +64,32 @@ data Bitcoin =
     , bitcoinRPC  :: BitcoinRPC
   } deriving Show
 
+-- | returns Just pid or Nothing if process has already exited
+getPid ph = withProcessHandle ph go
+  where
+    go ph_ = case ph_ of
+               OpenHandle x   -> return $ Just x
+               ClosedHandle _ -> return Nothing
+
+
 data BitcoinProc =
   BitcoinProc {
-      bitcoinStdout  :: Handle
-    , bitcoinProcess :: ProcessHandle
+      bitcoinStdout     :: Handle
+    , bitcoinProcess    :: ProcessHandle
+    , bitcoinProcessPID :: CPid
     }
 
-initBitcoin :: FilePath -> Word16 -> IO Bitcoin
-initBitcoin dir port = do
+instance Show BitcoinProc where
+  show BitcoinProc{..} =
+      printf "BitcoinProc [%d]" (fromIntegral bitcoinProcessPID :: Int)
+
+initBitcoin :: MonadIO m => FilePath -> Word16 -> m Bitcoin
+initBitcoin dir port = liftIO $ do
   let regtestdir  = dir        </> "regtest"
       regConfPath = regtestdir </> "bitcoin.conf"
       confPath    = dir        </> "bitcoin.conf"
       cmdline = [
-          "bitcoind"
-        , "-datadir=" ++ dir
+          "-datadir=" ++ dir
         , "-printtoconsole"
         , "-server"
         , "-regtest"
@@ -87,14 +117,49 @@ initBitcoin dir port = do
          , bitcoinArgs = cmdline
          })
 
-startBitcoin :: Bitcoin -> IO BitcoinProc
+
+startBitcoin :: (MonadLoggerIO m) => Bitcoin -> m BitcoinProc
 startBitcoin Bitcoin{..} = do
-  (_, Just stdout, _, procHandle) <- createProcess (P.proc "bitcoind" bitcoinArgs)
-  return $
-    BitcoinProc {
-      bitcoinStdout  = stdout
-    , bitcoinProcess = procHandle
+  let p = (P.proc "bitcoind" bitcoinArgs) { std_out = CreatePipe
+                                          , close_fds = False
+                                          }
+
+  (_, mstdout, _, procHandle) <- liftIO $ createProcess_ "bitcoind" p
+  mpid <- liftIO $ getPid procHandle
+
+  stdout <- maybe (fail "Could not open bitcoind stdout") return mstdout
+  pid    <- maybe (fail "Could not grab bitcoind pid") return mpid
+
+  let p = BitcoinProc {
+      bitcoinStdout     = stdout
+    , bitcoinProcess    = procHandle
+    , bitcoinProcessPID = fromIntegral pid
     }
+
+  $(logInfo) ("Starting " <> T.pack (show p))
+
+  return p
+
+
+log :: ByteString -> IO ()
+log = B8.putStrLn
+
+
+
+stopBitcoin :: MonadLoggerIO m => BitcoinProc -> m ()
+stopBitcoin b@BitcoinProc{..} = do
+  $(logInfo) ("Terminating " <> T.pack (show b))
+  liftIO $ terminateProcess bitcoinProcess
+  ma <- liftIO $ timeout (30 * 1000000) (waitForProcess bitcoinProcess)
+  maybe timedOut (return . const ()) ma
+  liftIO $ hClose bitcoinStdout
+  where
+    timedOut :: MonadLoggerIO m => m ()
+    timedOut = do
+      $(logInfo) "Bitcoin process timed out while try to close. Killing."
+      liftIO (signalProcess 9 bitcoinProcessPID)
+
+
 
 makeClient :: Manager -> ByteString -> Word16 -> ByteString -> ByteString -> BitcoinRPC
 makeClient manager host port user pass =
@@ -112,9 +177,21 @@ makeClient manager host port user pass =
   in BitcoinRPC manager req
 
 
+data JsonRPC a = JsonRPC {
+      jrpcResult :: a
+    , jrpcError  :: Maybe Text
+    , jrpcId     :: Int
+    }
+    deriving Show
 
-rpcCall :: (ToJSON a, FromJSON b) => BitcoinRPC -> Text -> a -> IO b
-rpcCall BitcoinRPC{..} method params =
+instance FromJSON a => FromJSON (JsonRPC a) where
+    parseJSON (Object obj) =
+        JsonRPC <$> obj .:  "result"
+                <*> obj .:? "error"
+                <*> obj .:  "id"
+
+call :: (ToJSON a, FromJSON b) => BitcoinRPC -> Text -> a -> IO b
+call BitcoinRPC{..} method params =
     let
         reqData =
             object [ "jsonrpc" .= T.pack "2.0"
@@ -130,9 +207,22 @@ rpcCall BitcoinRPC{..} method params =
       do
         res <- httpLbs req btcrpcManager
         let body = responseBody res
-        case JSON.decode body of
-          Nothing -> fail ("Could not decode JSON: \n\n" <> show body)
-          Just v  -> return v
+        case JSON.eitherDecode body of
+          Left e -> fail ("Could not decode JSON: \n\n" <> e <> "\n\n" <> show body )
+          Right res  -> return (jrpcResult res)
+
+getnewaddress :: BitcoinRPC -> IO Text
+getnewaddress rpc = call rpc "getnewaddress" (mempty :: Array)
+
+generatetoaddress :: BitcoinRPC -> Int -> Text -> IO [Text]
+generatetoaddress rpc nblocks addr =
+  call rpc "generatetoaddress" [toJSON nblocks, toJSON addr]
+
+generateBlocks :: BitcoinRPC -> Int -> IO [Text]
+generateBlocks rpc nblocks = do
+  addr <- getnewaddress rpc
+  generatetoaddress rpc 5 addr
+
 
 
 writeConfig :: FilePath -> Word16 -> IO ()
@@ -142,3 +232,20 @@ writeConfig file port = do
   hPutStrLn handle "rpcpassword=rpcpass"
   hPutStrLn handle ("rpcport=" <> B8.pack (show port))
   hClose handle
+
+withBitcoin :: (MonadUnliftIO m, MonadLoggerIO m)
+            => ((Bitcoin, BitcoinProc) -> m c) -> m c
+withBitcoin cb = UIO.bracket start stop cb
+  where
+    start = do
+      btc   <- initBitcoin "/tmp/bitcointest" 7888
+      bproc <- startBitcoin btc
+      return (btc, bproc)
+    stop (btc, bproc) = stopBitcoin bproc
+
+
+test :: IO ()
+test = runStderrLoggingT $ withBitcoin $ \(btc,_) -> do
+  let rpc = bitcoinRPC btc
+  addr <- liftIO (generateBlocks rpc 1)
+  liftIO (print addr)
